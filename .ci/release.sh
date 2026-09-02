833fcdc10c3ae41b5738f1c70a2b5fb1faca7ef6
Benoit Pierre <benoit.pierre@gmail.com>
2026-09-03 00:39:26 +0200

    tmp

--- .ci/release.sh
+++ .ci/release.sh
@@ -6,11 +6,10 @@ CI_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=/dev/null
 source "${CI_DIR}/common.sh"
 
-[[ -n "${GH_REPO}" ]]
-[[ -n "${GH_TOKEN}" ]]
-[[ -n "${GITHUB_REF}" ]]
-[[ $# -eq 1 ]]
-channel="$1"
+die() {
+    echo -e "${ANSI_RED}$*${ANSI_RESET}" 1>&2
+    exit 1
+}
 
 run() {
     echo -e "::group::${ANSI_GREEN}$(printf '%q ' "$@")${ANSI_RESET}" 1>&2
@@ -19,8 +18,14 @@ run() {
     return "${code}"
 }
 
-CONTAINER_ID="$(run docker run --detach --tty --volume="${PWD}/artifacts:/artifacts" --workdir=/artifacts/new "${DOCKER_IMAGE}" sh -c 'while true; do sleep 0.5; done')"
-trap 'run docker kill "${CONTAINER_ID}"' EXIT
+[[ $# -eq 0 ]] || "no arguments expected, got $#"
+[[ -n "${GH_REPO}" ]] || die '$GH_REPO is not set'
+[[ -n "${GH_TOKEN}" ]] || die '$GH_TOKEN is not set'
+[[ -n "${GITHUB_REF}" ]] || die '$GITHUB_REF is not set'
+[[ "${GITHUB_REF_TYPE}" = 'tag' ]] || "\$GITHUB_REF_TYPE is not tag: ${GITHUB_REF_TYPE}"
+
+# CONTAINER_ID="$(run docker run --detach --tty --volume="${PWD}/artifacts:/artifacts" --workdir=/artifacts/new "${DOCKER_IMAGE}" sh -c 'while true; do sleep 0.5; done')"
+# trap 'run docker kill "${CONTAINER_ID}"' EXIT
 
 container_exec() {
     echo -e "::group::docker exec … ${ANSI_GREEN}$(printf '%q ' "$@")${ANSI_RESET}" 1>&2
@@ -53,108 +58,108 @@ zsync_make() {
     container_exec "${cmd[@]}"
 }
 
-if [[ "${GITHUB_REF_TYPE}" = 'tag' ]]; then
+# if [[ "${GITHUB_REF_TYPE}" = 'tag' ]]; then
 
-    [[ -n "${GITHUB_REF_NAME}" ]]
-    channel='stable'
-    tag="${GITHUB_REF_NAME}"
-    draft=1
+#     [[ -n "${GITHUB_REF_NAME}" ]]
+#     channel='stable'
+#     tag="${GITHUB_REF_NAME}"
+#     draft=1
 
-else
+# else
 
-    channel='nightly'
-    tag="${channel}"
-    draft=''
+#     channel='nightly'
+#     tag="${channel}"
+#     draft=''
 
-fi
+# fi
 
-new_commit="$(git rev-parse HEAD)"
-old_commit="$(gh release view "${tag}" --json targetCommitish | jq -r .targetCommitish || true)"
-if [[ -n "${old_commit}" ]]; then
-    old_commit="$(git rev-parse "${old_commit}")"
-fi
+# new_commit="$(git rev-parse HEAD)"
+# old_commit="$(gh release view "${tag}" --json targetCommitish | jq -r .targetCommitish || true)"
+# if [[ -n "${old_commit}" ]]; then
+#     old_commit="$(git rev-parse "${old_commit}")"
+# fi
 
-create_release=0
-delete_release=0
+# create_release=0
+# delete_release=0
 
-if [[ -z "${old_commit}" ]]; then
-    create_release=1
-elif [[ "${old_commit}" != "${new_commit}" ]]; then
-    if gh release view "${tag}" --json assets | jq --exit-status '.assets[]|.name|select(test("\\.kotasync$"))'; then
-        run gh release download "${tag}" --dir artifacts/old --pattern '*.kotasync'
-    fi
-    create_release=1
-    delete_release=1
-fi
+# if [[ -z "${old_commit}" ]]; then
+#     create_release=1
+# elif [[ "${old_commit}" != "${new_commit}" ]]; then
+#     if gh release view "${tag}" --json assets | jq --exit-status '.assets[]|.name|select(test("\\.kotasync$"))'; then
+#         run gh release download "${tag}" --dir artifacts/old --pattern '*.kotasync'
+#     fi
+#     create_release=1
+#     delete_release=1
+# fi
 
-pushd artifacts/new || exit
-for a in *; do
-    case "${a}" in
-        koreader-*.AppImage)
-            arch="${a##*-}"
-            arch="${arch%.AppImage}"
-            printf %s "${a}" >"koreader-appimage-${arch}-latest-${channel}"
-            ;;
-        koreader-android-*.apk)
-            printf %s "${a}" >"${a%-v[0-9]*}-latest-${channel}"
-            ;;
-        koreader-kindlepw2-*.tar.xz)
-            kotasync_make "${a}" "${a%-v[0-9]*}-latest-${channel}.kotasync"
-            zsync_make "${a}" "${a%-v[0-9]*}-latest-${channel}.zsync"
-            ;;
-    esac
-done
-popd || exit
+# pushd artifacts/new || exit
+# for a in *; do
+#     case "${a}" in
+#         koreader-*.AppImage)
+#             arch="${a##*-}"
+#             arch="${arch%.AppImage}"
+#             printf %s "${a}" >"koreader-appimage-${arch}-latest-${channel}"
+#             ;;
+#         koreader-android-*.apk)
+#             printf %s "${a}" >"${a%-v[0-9]*}-latest-${channel}"
+#             ;;
+#         koreader-kindlepw2-*.tar.xz)
+#             kotasync_make "${a}" "${a%-v[0-9]*}-latest-${channel}.kotasync"
+#             zsync_make "${a}" "${a%-v[0-9]*}-latest-${channel}.zsync"
+#             ;;
+#     esac
+# done
+# popd || exit
 
-if [[ "${delete_release}" -ne 0 ]]; then
-    run gh release delete --cleanup-tag --yes "${tag}"
-fi
-if [[ "${delete_release}" -ne 0 ]] && [[ "${create_release}" -ne 0 ]]; then
-    # Workaround for https://github.com/cli/cli/issues/8458…
-    sleep 1
-fi
-if [[ "${create_release}" -ne 0 ]]; then
-    run git tag --force "${tag}"
-    run git push -f "${GH_REPO}" "refs/tags/${tag}"
-    run gh release create ${draft:+--draft} --notes='.' --prerelease --target="${new_commit}" --title="${tag}" "${tag}"
-fi
-artifacts=()
-for a in artifacts/new/*; do
-    case "${a##*/}" in
-        koreader-android-arm-*.apk)
-            a+='#Android ARM APK'
-            ;;
-        koreader-android-arm-*-${channel})
-            a+='#Android ARM OTA'
-            ;;
-        koreader-android-arm64-*.apk)
-            a+='#Android ARM64 APK'
-            ;;
-        koreader-android-arm64-*-${channel})
-            a+='#Android ARM64 OTA'
-            ;;
-        koreader-kindlepw2-*.tar.gz)
-            a+='#KindlePW2 TAR.GZ'
-            ;;
-        koreader-kindlepw2-*.tar.xz)
-            a+='#KindlePW2 TAR.XZ'
-            ;;
-        koreader-kindlepw2-*-${channel}.kotasync)
-            a+='#KindlePW2 OTA (KOTASync)'
-            ;;
-        koreader-kindlepw2-*-${channel}.zsync)
-            a+='#KindlePW2 OTA (ZSync)'
-            ;;
-        koreader-*-x86_64.AppImage)
-            a+='#Linux x86_64 AppImage'
-            ;;
-        koreader-appimage-*-${channel})
-            a+='#Linux x86_64 OTA'
-            ;;
-    esac
-    artifacts+=("${a}")
-done
-readarray -t artifacts < <(printf '%s\n' "${artifacts[@]}" | sort -t\# -k2)
-run gh release upload "${tag}" "${artifacts[@]}"
+# if [[ "${delete_release}" -ne 0 ]]; then
+#     run gh release delete --cleanup-tag --yes "${tag}"
+# fi
+# if [[ "${delete_release}" -ne 0 ]] && [[ "${create_release}" -ne 0 ]]; then
+#     # Workaround for https://github.com/cli/cli/issues/8458…
+#     sleep 1
+# fi
+# if [[ "${create_release}" -ne 0 ]]; then
+#     run git tag --force "${tag}"
+#     run git push -f "${GH_REPO}" "refs/tags/${tag}"
+#     run gh release create ${draft:+--draft} --notes='.' --prerelease --target="${new_commit}" --title="${tag}" "${tag}"
+# fi
+# artifacts=()
+# for a in artifacts/new/*; do
+#     case "${a##*/}" in
+#         koreader-android-arm-*.apk)
+#             a+='#Android ARM APK'
+#             ;;
+#         koreader-android-arm-*-${channel})
+#             a+='#Android ARM OTA'
+#             ;;
+#         koreader-android-arm64-*.apk)
+#             a+='#Android ARM64 APK'
+#             ;;
+#         koreader-android-arm64-*-${channel})
+#             a+='#Android ARM64 OTA'
+#             ;;
+#         koreader-kindlepw2-*.tar.gz)
+#             a+='#KindlePW2 TAR.GZ'
+#             ;;
+#         koreader-kindlepw2-*.tar.xz)
+#             a+='#KindlePW2 TAR.XZ'
+#             ;;
+#         koreader-kindlepw2-*-${channel}.kotasync)
+#             a+='#KindlePW2 OTA (KOTASync)'
+#             ;;
+#         koreader-kindlepw2-*-${channel}.zsync)
+#             a+='#KindlePW2 OTA (ZSync)'
+#             ;;
+#         koreader-*-x86_64.AppImage)
+#             a+='#Linux x86_64 AppImage'
+#             ;;
+#         koreader-appimage-*-${channel})
+#             a+='#Linux x86_64 OTA'
+#             ;;
+#     esac
+#     artifacts+=("${a}")
+# done
+# readarray -t artifacts < <(printf '%s\n' "${artifacts[@]}" | sort -t\# -k2)
+# run gh release upload "${tag}" "${artifacts[@]}"
 
-# vim: sw=4
+# # vim: sw=4
